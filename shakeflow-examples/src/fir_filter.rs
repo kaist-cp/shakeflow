0be392c958ce3436ab7cb4d241d51eeb2c3d58d8
Minseong Jang <minseong.jang@cp.kaist.ac.kr>
2022-11-09 17:03:05 +0900

    Fix IO prefix of FIR Filter

--- shakeflow-examples/src/fir_filter.rs
+++ shakeflow-examples/src/fir_filter.rs
@@ -6,22 +6,17 @@ pub type FirFilterChannel<const SZ: usize> = UniChannel<Bits<U<SZ>>>;
 pub fn fir_filter<const SZ: usize, const N: usize>(
     coeffs: [[bool; SZ]; N],
 ) -> Module<FirFilterChannel<SZ>, FirFilterChannel<SZ>> {
-    composite::<FirFilterChannel<SZ>, FirFilterChannel<SZ>, _>(
-        "fir_filter",
-        Some("input"),
-        Some("output"),
-        |value, k| {
-            value
-                .window::<N>(0.into(), k)
-                .map(k, move |value| {
-                    value.zip(Array::new(coeffs.into_iter().map(Bits::<U<SZ>>::from).collect()).into()).map(|p| {
-                        let (a, b) = *p;
-                        (a * b).resize()
-                    })
+    composite::<FirFilterChannel<SZ>, FirFilterChannel<SZ>, _>("fir_filter", Some("in"), Some("out"), |value, k| {
+        value
+            .window::<N>(0.into(), k)
+            .map(k, move |value| {
+                value.zip(Array::new(coeffs.into_iter().map(Bits::<U<SZ>>::from).collect()).into()).map(|p| {
+                    let (a, b) = *p;
+                    (a * b).resize()
                 })
-                .buffer(k, Expr::<Bits<U<SZ>>>::from(0).repeat())
-                .map(k, move |value| value.sum())
-        },
-    )
+            })
+            .buffer(k, Expr::<Bits<U<SZ>>>::from(0).repeat())
+            .map(k, move |value| value.sum())
+    })
     .build()
 }
